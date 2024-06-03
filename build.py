b9167f8baff303d73ab9d6f7bb4ba4f81b1d1d39
vlatan <33163699+vlatan@users.noreply.github.com>
2024-06-03 22:33:26 +0200

    Add css

--- build.py
+++ build.py
@@ -1,12 +1,25 @@
-from jinja2 import Template, Environment, FileSystemLoader, select_autoescape
+import shutil
+import pathlib
+import functools as ft
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 # load templates folder to environment (security measure)
 env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
 
-# load the `index.jinja` template
+# load the `index.html` template
 index_template = env.get_template("index.html")
 parsed_index_template = index_template.render(title="Hello World")
 
+
+static_dir_source_path = "static"
+copy_static_dir = ft.partial(shutil.copytree, static_dir_source_path, "build/static")
+
+try:
+    copy_static_dir()
+except FileExistsError as err:
+    shutil.rmtree("build/static")
+    copy_static_dir()
+
+
 # write the parsed template
-with open("build/index.html", "w") as home_page:
-    home_page.write(parsed_index_template)
+pathlib.Path("build/index.html").write_text(parsed_index_template)
