da1e72174073cd3dd25ec2b97a915f57862679eb
vlatan <33163699+vlatan@users.noreply.github.com>
2024-06-03 22:35:00 +0200

    Simplify static dir manipulation

--- build.py
+++ build.py
@@ -1,6 +1,5 @@
 import shutil
 import pathlib
-import functools as ft
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 # load templates folder to environment (security measure)
@@ -11,14 +10,11 @@ index_template = env.get_template("index.html")
 parsed_index_template = index_template.render(title="Hello World")
 
 
-static_dir_source_path = "static"
-copy_static_dir = ft.partial(shutil.copytree, static_dir_source_path, "build/static")
-
 try:
-    copy_static_dir()
+    shutil.copytree("static", "build/static")
 except FileExistsError as err:
     shutil.rmtree("build/static")
-    copy_static_dir()
+    shutil.copytree("static", "build/static")
 
 
 # write the parsed template
