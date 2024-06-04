60fa3fda302d95ef14246c2e1abd26264ac3c2a1
vlatan <33163699+vlatan@users.noreply.github.com>
2024-06-04 19:02:22 +0200

    Remove empty lines

--- build.py
+++ build.py
@@ -27,11 +27,9 @@ env = Environment(loader=FileSystemLoader(templates), autoescape=select_autoesca
 index_template = env.get_template("index.html")
 parsed_index_template = index_template.render(title="Hello World", config=config)
 
-
 static = pathlib.Path("themes") / theme / "static"
 if not static.is_dir():
     raise FileNotFoundError(f"No 'static' directory in your theme: '{static}'.")
-
 
 # include the static dir in the build
 try:
@@ -39,7 +37,6 @@ try:
 except FileExistsError as err:
     shutil.rmtree("build/static")
     shutil.copytree(static, "build/static")
-
 
 # write the parsed template
 pathlib.Path("build/index.html").write_text(parsed_index_template)
