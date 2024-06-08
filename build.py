9c71a626b3931e8d2b628c80cd7becfbd533b8c3
vlatan <33163699+vlatan@users.noreply.github.com>
2024-06-08 12:45:04 +0200

    Refactor into functions

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
-import os
 import shutil
-import pathlib
 import logging
+from pathlib import Path
 from slugify import slugify
 from markdown import Markdown
 from datetime import datetime
@@ -10,9 +9,6 @@ from dataclasses import dataclass
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from config import Config
-
-
-logging.getLogger().setLevel(logging.INFO)
 
 
 @dataclass(frozen=True)
@@ -42,7 +38,7 @@ class Post:
     content: str
 
 
-def parse_markdown(path: pathlib.Path) -> dict:
+def parse_markdown_file(path: Path, makrdown_instance: Markdown) -> dict:
     """Take post or page markdown filepath and return a dict of necessary data."""
 
     md_content = path.read_text().strip()
@@ -72,106 +68,130 @@ def parse_markdown(path: pathlib.Path) -> dict:
         data[key.lower()] = value
 
     # convert markdown to html
-    data["content"] = md.convert(content.strip())
+    data["content"] = makrdown_instance.convert(content.strip())
 
     return data
 
 
-def content_walk(path: pathlib.Path) -> dict:
+def content_walk(path: Path) -> tuple[list[Path], list[Path]]:
     """
     Walk a dir and gather specific filepaths (pages and posts).
     Also Copy/paste posts/images dir.
     """
 
-    result = {}
+    pages_paths, posts_paths = [], []
     for root, dirs, files in path.walk():
         if str(root) == "content/posts/images":
             shutil.copytree(root, "build/posts/images")
             continue
 
         if str(root) == "content/pages":
-            result["pages_paths"] = [root / fp for fp in files]
+            pages_paths = [root / fp for fp in files]
             continue
 
         if str(root) == "content/posts":
-            result["posts_paths"] = [root / fp for fp in files]
+            posts_paths = [root / fp for fp in files]
 
-    return result
-
-
-content_path = pathlib.Path("content")
-if not content_path.is_dir():
-    raise FileNotFoundError(f"No 'content' directory: '{content_path}'.")
-
-env_vars = {key: value for key, value in dotenv_values(".env").items() if value}
-cfg = Config(**env_vars)
-
-static_path = pathlib.Path("themes") / cfg.THEME / "static"
-if not static_path.is_dir():
-    raise FileNotFoundError(f"No 'static' directory in your theme: '{static_path}'.")
-
-if cfg.THEME == "default":
-    logging.info("Using the default theme.")
-
-templates = pathlib.Path("themes") / cfg.THEME / "templates"
-
-# load theme's templates folder to Jinja's environment
-jinja_env = Environment(
-    loader=FileSystemLoader(templates), autoescape=select_autoescape()
-)
-jinja_env.globals["config"] = cfg
-
-# remove the build directory
-shutil.rmtree(pathlib.Path("build"), ignore_errors=True)
-# include the static dir in the build
-shutil.copytree(static_path, "build/static")
+    return posts_paths, pages_paths
 
 
-md = Markdown(extensions=["toc"], output_format="html")
+def parse_content(posts_paths: list[Path], pages_paths: list[Path]) -> dict[str, set]:
+    """Parse all markdown content."""
+
+    posts, categories = set(), set()
+    for post_path in posts_paths:
+        data = parse_markdown_file(post_path, md)
+        post = Post(**data)
+        posts.add(post)
+        categories.add(data.get("category"))
+
+    pages, footer_pages = set(), set()
+    for page_path in pages_paths:
+        data = parse_markdown_file(page_path, md)
+        page = Page(**data)
+        if page.title in ["About", "Privacy"]:
+            footer_pages.add(page)
+        pages.add(page)
+
+    return {
+        "posts": posts,
+        "categories": categories,
+        "pages": pages,
+        "footer_pages": footer_pages,
+    }
 
 
-# TODO: Copy or move the favicons to root
+def render_content(posts: set, pages: set, jinja_env: Environment) -> None:
+    """Render all content to files."""
+
+    # render posts
+    for post in posts:
+        post_template = jinja_env.get_template("post.html")
+        parsed_post = post_template.render(post=post)
+        post_dir_path = Path(f"build/posts/{post.slug}")
+        post_dir_path.mkdir(exist_ok=True, parents=True)
+        Path(post_dir_path / "index.html").write_text(parsed_post)
+
+    # render pages
+    for page in pages:
+        page_template = jinja_env.get_template("page.html")
+        parsed_page = page_template.render(page=page)
+        page_dir_path = Path(f"build/pages/{page.slug}")
+        page_dir_path.mkdir(exist_ok=True, parents=True)
+        Path(page_dir_path / "index.html").write_text(parsed_page)
+
+    # render homepage
+    sorted_posts = sorted(posts, key=lambda x: x.date, reverse=True)
+    home_template = jinja_env.get_template("home.html")
+    parsed_home = home_template.render(posts=sorted_posts)
+    Path("build/index.html").write_text(parsed_home)
 
 
-content_filepaths = content_walk(content_path)
+if __name__ == "__main__":
 
-posts, categories = set(), set()
-for post_path in content_filepaths["posts_paths"]:
-    data = parse_markdown(post_path)
-    post = Post(**data)
-    posts.add(post)
-    categories.add(data.get("category"))
+    logging.getLogger().setLevel(logging.INFO)
 
-pages, footer_pages = set(), set()
-for page_path in content_filepaths["pages_paths"]:
-    data = parse_markdown(page_path)
-    page = Page(**data)
-    if page.title in ["About", "Privacy"]:
-        footer_pages.add(page)
-    pages.add(page)
+    content_path = Path("content")
+    if not content_path.is_dir():
+        raise FileNotFoundError(f"No 'content' directory: '{content_path}'.")
 
-jinja_env.globals["categories"] = sorted(categories, key=lambda x: x.name)
-jinja_env.globals["footer_pages"] = sorted(footer_pages, key=lambda x: x.title)
+    env_vars = {key: value for key, value in dotenv_values(".env").items() if value}
+    cfg = Config(**env_vars)
 
-for post in posts:
-    post_template = jinja_env.get_template("post.html")
-    parsed_post = post_template.render(post=post)
-    post_dir_path = pathlib.Path(f"build/posts/{post.slug}")
-    post_dir_path.mkdir(exist_ok=True, parents=True)
-    pathlib.Path(post_dir_path / "index.html").write_text(parsed_post)
+    static_path = Path("themes") / cfg.THEME / "static"
+    if not static_path.is_dir():
+        msg = f"No 'static' directory in your theme: '{static_path}'."
+        raise FileNotFoundError(msg)
 
-for page in pages:
-    page_template = jinja_env.get_template("page.html")
-    parsed_page = page_template.render(page=page)
-    page_dir_path = pathlib.Path(f"build/pages/{page.slug}")
-    page_dir_path.mkdir(exist_ok=True, parents=True)
-    pathlib.Path(page_dir_path / "index.html").write_text(parsed_page)
+    if cfg.THEME == "default":
+        logging.info("Using the default theme.")
 
-# create homepage
-posts = sorted(posts, key=lambda x: x.date, reverse=True)
-home_template = jinja_env.get_template("home.html")
-parsed_home = home_template.render(posts=posts)
-pathlib.Path("build/index.html").write_text(parsed_home)
+    # load theme's templates folder to Jinja's environment
+    templates_path = Path("themes") / cfg.THEME / "templates"
+    loader = FileSystemLoader(templates_path)
+    jinja_env = Environment(loader=loader, autoescape=select_autoescape())
+    jinja_env.globals["config"] = cfg
 
-# TODO: aotoversion filter
-# TODO: category content pages
+    # remove the build directory
+    shutil.rmtree(Path("build"), ignore_errors=True)
+    # include the static dir in the build
+    shutil.copytree(static_path, "build/static")
+
+    # create a markdown instance
+    md = Markdown(extensions=["toc"], output_format="html")
+
+    posts_paths, pages_paths = content_walk(content_path)
+    parsed_content = parse_content(posts_paths, pages_paths)
+
+    categories = parsed_content["categories"]
+    footer_pages = parsed_content["footer_pages"]
+
+    jinja_env.globals["categories"] = sorted(categories, key=lambda x: x.name)
+    jinja_env.globals["footer_pages"] = sorted(footer_pages, key=lambda x: x.title)
+
+    posts, pages = parsed_content["posts"], parsed_content["pages"]
+    render_content(posts, pages, jinja_env)
+
+    # TODO: Copy or move the favicons to root
+    # TODO: aotoversion filter
+    # TODO: category pages
