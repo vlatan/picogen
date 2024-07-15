bd1f0508db6dbef1e268f51bc5185a1b688b496c
vlatan <33163699+vlatan@users.noreply.github.com>
2024-07-15 20:29:23 +0200

    Package the project

--- picogen/__main__.py
+++ picogen/__main__.py
@@ -10,7 +10,7 @@ from dataclasses import dataclass
 from itertools import zip_longest
 from datetime import datetime, date
 
-from config import Config
+from picogen.config import Config
 
 
 @dataclass(frozen=True)
@@ -93,8 +93,8 @@ def content_walk(path: Path) -> tuple[list[Path], list[Path]]:
 
     pages_paths, posts_paths, posts_dirs, pages_dirs = [], [], [], []
     for root, dirs, files in path.walk():
-        pages_dirs += [root / dr for dr in dirs if str(root) == "content/pages"]
-        posts_dirs += [root / dr for dr in dirs if str(root) == "content/posts"]
+        pages_dirs += [root / dr for dr in dirs if str(root).endswith("content/pages")]
+        posts_dirs += [root / dr for dr in dirs if str(root).endswith("content/posts")]
         posts_paths += [root / fp for fp in files if root in posts_dirs]
         pages_paths += [root / fp for fp in files if root in pages_dirs]
 
@@ -102,6 +102,7 @@ def content_walk(path: Path) -> tuple[list[Path], list[Path]]:
 
 
 def render_content(
+    build_dir: Path,
     posts_paths: list[Path],
     pages_paths: list[Path],
     makrdown_instance: Markdown,
@@ -120,7 +121,7 @@ def render_content(
 
             # create post dir in the build and copy its images dir if any
             src_post_images = post_path.parent / "images"
-            dst_post_dir = Path(f"build/posts/{post.slug}")
+            dst_post_dir = build_dir / "posts" / post.slug
             dst_post_dir.mkdir(exist_ok=True, parents=True)
             if src_post_images.exists():
                 shutil.copytree(src_post_images, dst_post_dir / "images")
@@ -133,7 +134,7 @@ def render_content(
 
             # create page dir in the build and copy its images dir if any
             src_page_images = page_path.parent / "images"
-            dst_page_dir = Path(f"build/pages/{page.slug}")
+            dst_page_dir = build_dir / "pages" / page.slug
             dst_page_dir.mkdir(exist_ok=True, parents=True)
             if src_page_images.exists():
                 shutil.copytree(src_page_images, dst_page_dir / "images")
@@ -146,71 +147,84 @@ def render_content(
         if post:
             post_template = jinja_env.get_template("post.html")
             parsed_post = post_template.render(post=post)
-            post_dir_path = Path(f"build/posts/{post.slug}")
+            post_dir_path = build_dir / "posts" / post.slug
             Path(post_dir_path / "index.html").write_text(parsed_post)
 
         if page:
             page_template = jinja_env.get_template("page.html")
             parsed_page = page_template.render(page=page)
-            page_dir_path = Path(f"build/pages/{page.slug}")
+            page_dir_path = build_dir / "pages" / page.slug
             Path(page_dir_path / "index.html").write_text(parsed_page)
 
         if category:
             cat_template = jinja_env.get_template("category.html")
             parsed_cat = cat_template.render(category=category)
-            cat_dir_path = Path(f"build/categories/{category.slug}")
+            cat_dir_path = build_dir / "categories" / category.slug
             cat_dir_path.mkdir(exist_ok=True, parents=True)
             Path(cat_dir_path / "index.html").write_text(parsed_cat)
 
     # render homepage
     home_template = jinja_env.get_template("home.html")
     parsed_home = home_template.render()
-    Path("build/index.html").write_text(parsed_home)
+    (build_dir / "index.html").write_text(parsed_home)
 
     # render 404 page
     not_found_template = jinja_env.get_template("404.html")
     parsed_not_found = not_found_template.render()
-    Path("build/404.html").write_text(parsed_not_found)
+    (build_dir / "404.html").write_text(parsed_not_found)
 
     # render robots.txt
     robots_template = jinja_env.get_template("robots.txt")
     parsed_robots = robots_template.render()
-    Path("build/robots.txt").write_text(parsed_robots)
+    (build_dir / "robots.txt").write_text(parsed_robots)
 
     # render sitemap.xml
     xml_template = jinja_env.get_template("sitemap.xml")
     parsed_xml = xml_template.render()
-    Path("build/sitemap.xml").write_text(parsed_xml)
+    (build_dir / "sitemap.xml").write_text(parsed_xml)
 
     # try to render sitemap.xsl
     try:
         xsl_template = jinja_env.get_template("sitemap.xsl")
         parsed_xsl = xsl_template.render()
-        Path("build/sitemap.xsl").write_text(parsed_xsl)
+        (build_dir / "sitemap.xsl").write_text(parsed_xsl)
     except jinja.exceptions.TemplateNotFound:
         pass
 
 
-def main() -> None:
+def build() -> None:
+    """Build the static website."""
+
+    # set logging level
     logging.getLogger().setLevel(logging.INFO)
 
-    content_path = Path("content")
+    # find out the current working directory
+    working_dir = Path.cwd()
+
+    content_path = working_dir / "content"
     if not content_path.is_dir():
         raise FileNotFoundError(f"No 'content' directory: '{content_path}'.")
 
-    env_vars = {key: value for key, value in dotenv_values(".env").items() if value}
+    env_file = working_dir / ".env"
+    env_vars = {key: value for key, value in dotenv_values(env_file).items() if value}
     cfg = Config(**env_vars)
 
-    static_path = Path("themes") / cfg.THEME / "static"
+    theme = working_dir / "themes" / cfg.THEME
+    if cfg.THEME == "default":
+        theme = Path(__file__).resolve().parent / "default_theme"
+        logging.info("Using the default theme.")
+
+    static_path = theme / "static"
     if not static_path.is_dir():
         msg = f"No 'static' directory in your theme: '{static_path}'."
         raise FileNotFoundError(msg)
 
-    if cfg.THEME == "default":
-        logging.info("Using the default theme.")
+    templates_path = theme / "templates"
+    if not templates_path.is_dir():
+        msg = f"No 'templates' directory in your theme: '{templates_path}'."
+        raise FileNotFoundError(msg)
 
     # load theme's templates folder to Jinja's environment
-    templates_path = Path("themes") / cfg.THEME / "templates"
     loader = jinja.FileSystemLoader(templates_path)
     jinja_env = jinja.Environment(
         trim_blocks=True,
@@ -223,19 +237,21 @@ def main() -> None:
     jinja_env.filters["autoversion"] = autoversion_file
 
     # remove the build directory
-    shutil.rmtree(Path("build"), ignore_errors=True)
+    build_dir = working_dir / "build"
+    shutil.rmtree(build_dir, ignore_errors=True)
     # include the static dir in the build
-    shutil.copytree(static_path, "build/static")
+    shutil.copytree(static_path, build_dir / "static")
     # include the favicons in the root dir too
-    shutil.copytree(static_path / "favicons", "build", dirs_exist_ok=True)
+    shutil.copytree(static_path / "favicons", build_dir, dirs_exist_ok=True)
 
     # get posts and pages path in content dir
     posts_paths, pages_paths = content_walk(content_path)
+
     # create markdown instance
     md = Markdown(extensions=["toc", "codehilite", "extra"], output_format="html")
     # parse and render all content
-    render_content(posts_paths, pages_paths, md, jinja_env)
+    render_content(build_dir, posts_paths, pages_paths, md, jinja_env)
 
 
 if __name__ == "__main__":
-    main()
+    build()
