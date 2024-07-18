dfb155b90bbfeeaf4fa5625b680fb75191c87ffa
vlatan <33163699+vlatan@users.noreply.github.com>
2024-07-18 21:14:08 +0200

    Dynamic versioning

--- src/picogen/__init__.py
+++ src/picogen/__init__.py
@@ -1,3 +1,8 @@
+"""Minimalist static website generator in Python."""
+
+__version__ = "0.0.17"
+
+
 from .deploy import deploy_website
 from .backup import backup_content
 from .generate import generate_website
