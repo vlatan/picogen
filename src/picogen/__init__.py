83f217ba0c19a2db6d5acf0ad07df5db489e6f87
vlatan <33163699+vlatan@users.noreply.github.com>
2025-10-08 14:54:28 +0200

    Get version from tag

--- src/picogen/__init__.py
+++ src/picogen/__init__.py
@@ -1,8 +1,5 @@
 """Minimalist static website generator in Python."""
 
-__version__ = "0.0.20"
-
-
 from .deploy import deploy_website
 from .backup import backup_content
 from .generate import generate_website
